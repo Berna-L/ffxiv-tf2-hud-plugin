66af40f995b9d3ee96d540134d5721c0db710c02
Bernardo Lopes <bernardo.lopes@outlook.com>
2023-01-18 23:22:38 -0300

    Fixes Winforms error (goatcorp/DalamudPluginsD17#1155).
    At least it's what I hope.

--- TF2CriticalHitsPlugin/SoundEngine.cs
+++ TF2CriticalHitsPlugin/SoundEngine.cs
@@ -1,60 +1,55 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
+using Dalamud.Logging;
 using Dalamud.Utility;
+using Lumina.Data;
 using NAudio.Wave;
 
 namespace Tf2CriticalHitsPlugin;
 
-public class SoundEngine
+public static class SoundEngine
 {
-    // Copied from PeepingTom plugin
-    public static void PlaySound(string path, float volume = 1.0f)
+    // Copied from PeepingTom plugin, by ascclemens:
+    // https://git.anna.lgbt/ascclemens/PeepingTom/src/commit/3749a6b42154a51397733abb2d3b06a47915bdcc/Peeping%20Tom/TargetWatcher.cs#L162
+    public static void PlaySound(string? path, float volume = 1.0f)
     {
         if (path.IsNullOrEmpty() || !File.Exists(path))
         {
-            Dalamud.Logging.PluginLog.Error($"Could not find file: {path}");
-            return;
+            PluginLog.Error($"Could not find file: {path}");
         }
-
-        var soundDevice = -1;
-        if (soundDevice < -1 || soundDevice > WaveOut.DeviceCount)
-        {
-            soundDevice = -1;
-        }
-
+        
+        var soundDevice = DirectSoundOut.DSDEVID_DefaultPlayback;
         new Thread(() => {
             WaveStream reader;
-            try
-            {
-                reader = new AudioFileReader(path);
-            }
-            catch (Exception e)
-            {
-                Dalamud.Logging.PluginLog.Error($"Could not play sound file: {e.Message}");
+            try {
+                reader = new MediaFoundationReader(path);
+            } catch (Exception e) {
+                PluginLog.LogError(e.Message);
                 return;
             }
-                
-            using WaveChannel32 channel = new(reader)
-            {
+
+            using var channel = new WaveChannel32(reader) {
                 Volume = volume,
                 PadWithZeroes = false,
             };
 
-            using (reader)
-            {
-                using var output = new WaveOutEvent
-                {
-                    DeviceNumber = soundDevice,
-                };
-                output.Init(channel);
-                output.Play();
+            using (reader) {
+                using var output = new DirectSoundOut(soundDevice);
 
-                while (output.PlaybackState == PlaybackState.Playing)
-                {
-                    Thread.Sleep(500);
+                try {
+                    output.Init(channel);
+                    output.Play();
+
+                    while (output.PlaybackState == PlaybackState.Playing) {
+                        Thread.Sleep(500);
+                    }
+                } catch (Exception ex) {
+                    PluginLog.LogError(ex, "Exception playing sound");
                 }
             }
         }).Start();
+        
     }
 }
