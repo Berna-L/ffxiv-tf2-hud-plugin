b25e2642f974fe1fe32432add804732076205c36
Bernardo Lopes <bernardo.lopes@outlook.com>
2023-03-18 16:02:57 -0300

    Add Rider Source Template to log things quicker.

---
+++ Tf2CriticalHitsPlugin/Common/Template/RiderSourceTemplates.cs
@@ -0,0 +1,13 @@
+﻿using Dalamud.Logging;
+using JetBrains.Annotations;
+
+namespace Tf2CriticalHitsPlugin.Common.Template;
+
+public static class RiderSourceTemplates
+{
+    [SourceTemplate]
+    public static void log(this object obj)
+    {
+        PluginLog.Debug(obj.ToString());
+    } 
+}
