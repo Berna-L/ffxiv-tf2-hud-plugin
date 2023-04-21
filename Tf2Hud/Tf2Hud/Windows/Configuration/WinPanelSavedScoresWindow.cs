bf51c018505fa31af5bcb83c826b277f762c6030
Bernardo Lopes <bernardo.lopes@outlook.com>
2023-04-21 19:31:41 -0300

    #8: Add CSV export to the saved scores window.

--- Tf2Hud/Tf2Hud/Windows/Configuration/WinPanelSavedScoresWindow.cs
+++ Tf2Hud/Tf2Hud/Windows/Configuration/WinPanelSavedScoresWindow.cs
@@ -5,9 +5,10 @@ using ImGuiNET;
 using Lumina.Excel.GeneratedSheets;
 using Tf2Hud.Common.Configuration;
 using System;
+using System.Text;
+using System.Windows.Forms;
 using Dalamud.Interface;
 using Dalamud.Interface.Components;
-using Dalamud.Logging;
 using KamiLib.Drawing;
 
 namespace Tf2Hud.Tf2Hud.Windows.Configuration;
@@ -21,8 +22,40 @@ public class WinPanelSavedScoresWindow: Window
     }
     public override void Draw()
     {
+        DrawScoresTable();
+        DrawCsvButton();
+    }
+
+    private void DrawCsvButton()
+    {
+        ImGui.SetCursorPosY(ImGui.GetCursorPosY() + ImGui.GetContentRegionAvail().Y - (ImGui.GetTextLineHeight() + 10));
+        if (ImGui.Button("Copy as CSV to Clipboard"))
+        {
+            Clipboard.SetText(GetScoresAsCsv());
+        }
+    }
+
+    private string GetScoresAsCsv()
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine("Territory ID,Duty Name,Player Score,Enemy Score");
+        foreach (var (duty, score) in winPanelConfigZero.SavedScores.OrderBy(s => s.Key))
+        {
+            stringBuilder.Append(duty);
+            stringBuilder.Append(',');
+            stringBuilder.Append(GetDuty(duty));
+            stringBuilder.Append(',');
+            stringBuilder.Append(score.PlayerTeam);
+            stringBuilder.Append(',');
+            stringBuilder.Append(score.EnemyTeam);
+            stringBuilder.AppendLine();
+        }
+        return stringBuilder.ToString();
+    }
+
+    private void DrawScoresTable()
+    {
         using var _ = ImRaii.Table("##ScoresTable", 4);
-        
         ImGui.TableNextColumn();
         ImGui.TableHeader("Duty");
         ImGui.TableNextColumn();
